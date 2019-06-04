6176c2f898313bd22864ee72e07a2102f1444da5
webhacking <a@hax0r.info>
2019-06-04 18:38:12 +0900

    Version patchs

--- dist/remote-screen-capture.d.ts
+++ dist/remote-screen-capture.d.ts
@@ -1,6 +1,6 @@
 import { ThenableWebDriver } from 'selenium-webdriver';
 import { Observable } from 'rxjs';
-export declare class RemoteScreenCapture {
+export default class RemoteScreenCapture {
     getDriver(): Observable<ThenableWebDriver>;
     static take(URI: string, savePath?: string): Observable<string | void>;
 }
--- dist/remote-screen-capture.js
+++ dist/remote-screen-capture.js
@@ -1,32 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-const chrome = require("selenium-webdriver/chrome");
-const chromeDriver = require("chromedriver");
-const selenium_webdriver_1 = require("selenium-webdriver");
-const rxjs_1 = require("rxjs");
-const operators_1 = require("rxjs/operators");
-class RemoteScreenCapture {
-    getDriver() {
+import * as fs from 'fs';
+import * as chrome from 'selenium-webdriver/chrome';
+import * as chromeDriver from 'chromedriver';
+import { Builder, Capabilities } from 'selenium-webdriver';
+import { from, of } from 'rxjs';
+import { concatMap, map, tap } from 'rxjs/operators';
+var RemoteScreenCapture = (function () {
+    function RemoteScreenCapture() {
+    }
+    RemoteScreenCapture.prototype.getDriver = function () {
         chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
-        return rxjs_1.of(new selenium_webdriver_1.Builder()
-            .withCapabilities(selenium_webdriver_1.Capabilities.chrome())
+        return of(new Builder()
+            .withCapabilities(Capabilities.chrome())
             .setChromeOptions(new chrome.Options()
             .addExtensions())
             .build());
-    }
-    static take(URI, savePath) {
-        return (new RemoteScreenCapture).getDriver().pipe(operators_1.concatMap((driver) => {
-            return rxjs_1.from(driver.get(URI)).pipe(operators_1.concatMap(() => rxjs_1.from(driver.takeScreenshot())), operators_1.tap(() => rxjs_1.from(driver.close())));
-        }), operators_1.map((encodedPngChunks) => {
+    };
+    RemoteScreenCapture.take = function (URI, savePath) {
+        return (new RemoteScreenCapture).getDriver().pipe(concatMap(function (driver) {
+            return from(driver.get(URI)).pipe(concatMap(function () { return from(driver.takeScreenshot()); }), tap(function () { return from(driver.close()); }));
+        }), map(function (encodedPngChunks) {
             if (savePath) {
-                const absoluteSavePath = `${savePath}/${new Date().getTime()}.png`;
+                var absoluteSavePath = savePath + "/" + new Date().getTime() + ".png";
                 fs.writeFileSync(absoluteSavePath, encodedPngChunks, 'base64');
                 return absoluteSavePath;
             }
             return encodedPngChunks;
         }));
-    }
-}
-exports.RemoteScreenCapture = RemoteScreenCapture;
+    };
+    return RemoteScreenCapture;
+}());
+export default RemoteScreenCapture;
 //# sourceMappingURL=remote-screen-capture.js.map
