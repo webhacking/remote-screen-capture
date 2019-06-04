843f29ece37dfc0183852a16fae11594d8cf6777
webhacking <a@hax0r.info>
2019-06-04 18:48:55 +0900

    Version up

--- dist/remote-screen-capture.js
+++ dist/remote-screen-capture.js
@@ -4,30 +4,26 @@ import * as chromeDriver from 'chromedriver';
 import { Builder, Capabilities } from 'selenium-webdriver';
 import { from, of } from 'rxjs';
 import { concatMap, map, tap } from 'rxjs/operators';
-var RemoteScreenCapture = (function () {
-    function RemoteScreenCapture() {
-    }
-    RemoteScreenCapture.prototype.getDriver = function () {
+export default class RemoteScreenCapture {
+    getDriver() {
         chrome.setDefaultService(new chrome.ServiceBuilder(chromeDriver.path).build());
         return of(new Builder()
             .withCapabilities(Capabilities.chrome())
             .setChromeOptions(new chrome.Options()
             .addExtensions())
             .build());
-    };
-    RemoteScreenCapture.take = function (URI, savePath) {
-        return (new RemoteScreenCapture).getDriver().pipe(concatMap(function (driver) {
-            return from(driver.get(URI)).pipe(concatMap(function () { return from(driver.takeScreenshot()); }), tap(function () { return from(driver.close()); }));
-        }), map(function (encodedPngChunks) {
+    }
+    static take(URI, savePath) {
+        return (new RemoteScreenCapture).getDriver().pipe(concatMap((driver) => {
+            return from(driver.get(URI)).pipe(concatMap(() => from(driver.takeScreenshot())), tap(() => from(driver.close())));
+        }), map((encodedPngChunks) => {
             if (savePath) {
-                var absoluteSavePath = savePath + "/" + new Date().getTime() + ".png";
+                const absoluteSavePath = `${savePath}/${new Date().getTime()}.png`;
                 fs.writeFileSync(absoluteSavePath, encodedPngChunks, 'base64');
                 return absoluteSavePath;
             }
             return encodedPngChunks;
         }));
-    };
-    return RemoteScreenCapture;
-}());
-export default RemoteScreenCapture;
+    }
+}
 //# sourceMappingURL=remote-screen-capture.js.map
